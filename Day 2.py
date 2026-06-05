7f688058d85c26f2193b6757795bc8d8be5f53e7
Ruheli <ruhelighosh@gmail.com>
2026-06-05 21:29:28 +0530

    Implement LCM calculation for two numbers

--- Day 2.py
+++ Day 2.py
@@ -1,8 +1,15 @@
-#find GCD of two numbers
-a=int(input("enter first number"))
-b=int(input("enter second number"))
-while b!=0:
-    a,b=b,a%b
-    print("GCD is",a)
+#find LCM of two numbers
+num1=int(input("enter first number"))
+num2=int(input("enter a second number"))
+if num1>num2:
+    greater=num1
+else:
+    greater=num2
+    while True:
+        if greater%num1==0 and greater%num2==0:
+            lcm=greater
+            break
+        greater+=1
+    print("LCM of",num1,"and",num2,"is,",lcm)
 
         

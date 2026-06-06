28bdb776e9670ac785ff0bbcdc88efdb4f41c0b5
Ruheli <ruhelighosh@gmail.com>
2026-06-06 21:31:36 +0530

    day3

--- Day3.py
+++ Day3.py
@@ -1,15 +1,14 @@
 
-#to check armstrong number
-num = int(input("Enter a number: "))
-sum = 0
-temp = num
-while temp > 0:
-    digit = temp % 10
-    sum += digit ** 3
-    temp //= 10 
-if num == sum:
-    print(num, "is an armstrong number")    
-else:
-    print(num, "is not an armstrong number")
-
-    
+#print armstrong number in range
+start=int(input("enter the starting number :"))
+end=int(input("enter the ending number :"))
+for num in range(start,end+1):
+    temp=num
+    digits=len(str(num))
+    total=0
+    while temp>0:
+        digit=temp%10
+        total+=digit**digits
+        temp//=10
+    if num==total:
+        print(num)

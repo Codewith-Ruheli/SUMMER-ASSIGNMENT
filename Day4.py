e9ddac1d43de394fc07948cc1320dd6bcab1a3e3
Ruheli <ruhelighosh@gmail.com>
2026-06-07 18:34:56 +0530

    day4 Q2

---
+++ Day4.py
@@ -0,0 +1,17 @@
+
+#to check strong number
+n=int(input("enter a number :"))
+temp=n
+sum=0
+while temp>0:
+    digit=temp%10
+    fact=1
+    for i in range(1,digit+1):
+        fact=fact*i
+        sum=sum+fact
+        temp=temp//10
+        if sum==n:
+            print("strongnumber :")
+        else:
+            print("not a strong number :")
+            
